dacbc6cfd4cd3fc3ca83134898f16eb57dd2fb39
James <jwu277@gmail.com>
2018-11-05 15:47:20 -0800

    Completed implementation and passed provided test cases

--- src/main/java/fibonacciTransform/FibTransform.java
+++ src/main/java/fibonacciTransform/FibTransform.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 
 public class FibTransform {
 
+    private static int numDoublings = 0;
+
 	/**
 	 * Transform a number into a Fibonacci number using at most m steps: Of
 	 * these steps, one can be a doubling step, and the other steps increase
@@ -21,8 +23,15 @@ public class FibTransform {
 	 * @return true if the transformation is possible and false otherwise
 	 */
 	public static boolean isPossible_onlyOneDoubling(int n, int m) {
-		// TODO: Implement this method
-		return false; // change this
+	    
+        if (n == 0) {
+            return true;
+        }
+
+        numDoublings = 0;
+
+		return isPossible(n, m) && numDoublings <= 1;
+
 	}
 
 	/**
@@ -40,8 +49,51 @@ public class FibTransform {
 	 * @return true if the transformation is possible and false otherwise
 	 */
 	public static boolean isPossible(int n, int m) {
-		// TODO: Implement this method
-		return false; // change this
+
+	    if (n == 0) {
+	        return true;
+        }
+
+		if (isFib(n)) {
+		    return true;
+        } else if (m == 0) {
+		    return false;
+        } else {
+		    if (isPossible(n + 1, m - 1)) {
+		        return true;
+            } else if (isPossible(2 * n, m - 1)) {
+		        numDoublings++;
+		        return true;
+            } else {
+		        return false;
+            }
+        }
+
 	}
+
+	private static boolean isFib(int k) {
+
+	    int n2 = 1;
+	    int n1 = 1;
+	    int n = 2;
+
+	    int temp;
+
+	    while (true) {
+
+	        if (n == k) {
+	            return true;
+            } else if (n > k) {
+	            return false;
+            }
+
+            temp = n + n1;
+	        n2 = n1;
+	        n1 = n;
+	        n = temp;
+
+        }
+
+    }
 
 }
